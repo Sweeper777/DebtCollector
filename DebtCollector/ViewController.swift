cd3a29f1895b7687be0499c23a73a33d8042d6c5
Sweeper777 <sumulang@gmail.com>
2018-07-10 09:48:44 +0100

    Initial Commit

---
+++ DebtCollector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DebtCollector
+//
+//  Created by Mulang Su on 2018/07/10.
+//  Copyright © 2018年 Mulang Su. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
