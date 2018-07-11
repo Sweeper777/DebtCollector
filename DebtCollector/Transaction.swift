ef3ffeaf203133f35884776b549d47ca1b022bc7
Sweeper777 <sumulang@gmail.com>
2018-07-11 10:21:32 +0100

    added GroupTransaction model class

--- DebtCollector/Transaction.swift
+++ DebtCollector/Transaction.swift
@@ -4,3 +4,9 @@ class Transaction: Object {
     @objc dynamic var amount: Double = 0.0
     @objc dynamic var name = ""
 }
+
+class GroupTransaction: Object {
+    @objc dynamic var title = ""
+    @objc dynamic var details = ""
+    let value = List<Transaction>()
+}
