f2db8afdfd9f0bfa30f5280556873c3f6779f35b
Sweeper777 <sumulang@gmail.com>
2018-07-13 10:18:51 +0100

    rewrote parts of the model

--- DebtCollector/Transaction.swift
+++ DebtCollector/Transaction.swift
@@ -2,13 +2,16 @@ import RealmSwift
 
 class Transaction: Object {
     @objc dynamic var amount: Double = 0.0
-    @objc dynamic var name = ""
+    @objc dynamic var person: Person?
+    @objc dynamic var date = Date()
+    let parentTransactions = LinkingObjects(fromType: GroupTransaction.self, property: "transactions")
 }
 
 class GroupTransaction: Object {
     @objc dynamic var title = ""
     @objc dynamic var details = ""
-    let value = List<Transaction>()
+    let transactions = List<Transaction>()
+}
 
 class Person : Object {
     @objc dynamic var name = ""
