f106e7abe1b3582d931bea28e553218d1c3000f0
Sweeper777 <sumulang@gmail.com>
2018-07-14 08:55:10 +0100

    binded people data to table view

--- DebtCollector/PeopleViewController.swift
+++ DebtCollector/PeopleViewController.swift
@@ -6,4 +6,12 @@ import SCLAlertView
 class PeopleViewController: UITableViewController {
     let disposeBag = DisposeBag()
     
+    override func viewDidLoad() {
+        RealmWrapper.shared.people.asObservable()
+            .bind(to: tableView.rx.items(cellIdentifier: "cell")) {
+                index, person, cell in
+                cell.textLabel?.text = person.name
+        }.disposed(by: disposeBag)
+    }
+    
 }
