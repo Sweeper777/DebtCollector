64fb18fe9be9f18de53ce06b5fce675fb326a5e4
Sweeper777 <sumulang@gmail.com>
2018-07-15 09:40:54 +0100

    added alert buttons for add people alert

--- DebtCollector/PeopleViewController.swift
+++ DebtCollector/PeopleViewController.swift
@@ -17,11 +17,15 @@ class PeopleViewController: UITableViewController {
     @IBAction func addPerson() {
         let alert = SCLAlertView(appearance: SCLAlertView.SCLAppearance(showCloseButton: false))
         let nameField = alert.addTextField("Name")
+        alert.addButton("OK", action: {
+            let newPerson = Person()
+            newPerson.name = nameField.text!
+            try! RealmWrapper.shared.realm.write {
+                RealmWrapper.shared.realm.add(newPerson)
+            }
+        })
+        alert.addButton("Cancel", action: {})
         alert.showEdit("New Person", subTitle: "Please enter the name of the person:")
-        let newPerson = Person()
-        newPerson.name = nameField.text!
-        try! RealmWrapper.shared.realm.write {
-            RealmWrapper.shared.realm.add(newPerson)
-        }
+        
     }
 }
