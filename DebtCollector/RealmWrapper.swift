c663b22ec59e294ef17e575abb83f9f564ce68dd
Sweeper777 <sumulang@gmail.com>
2018-07-15 09:40:34 +0100

    stopped using the deprecated Variable type

--- DebtCollector/RealmWrapper.swift
+++ DebtCollector/RealmWrapper.swift
@@ -2,16 +2,16 @@ import RealmSwift
 import RxSwift
 
 final class RealmWrapper {
-    let transactions: Variable<Results<Transaction>>
-    let groupTransactions: Variable<Results<GroupTransaction>>
-    let people: Variable<Results<Person>>
+    let transactions: Results<Transaction>
+    let groupTransactions: Results<GroupTransaction>
+    let people: Results<Person>
     let realm: Realm!
     
     private init() {
         realm = try! Realm()
-        transactions = Variable(realm.objects(Transaction.self))
-        groupTransactions = Variable(realm.objects(GroupTransaction.self))
-        people = Variable(realm.objects(Person.self))
+        transactions = realm.objects(Transaction.self)
+        groupTransactions = realm.objects(GroupTransaction.self)
+        people = realm.objects(Person.self)
     }
     
     static let shared = RealmWrapper()
