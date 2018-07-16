302e1f8840cb2620c6b7cff77eac4496e5df6d8c
Sweeper777 <sumulang@gmail.com>
2018-07-16 13:33:57 +0100

    added BriefTransactionTableViewCell.swift

---
+++ DebtCollector/BriefTransactionTableViewCell.swift
@@ -0,0 +1,7 @@
+import UIKit
+class BriefTransactionTableViewCell: UITableViewCell {
+    @IBOutlet var transactionLabel: UILabel!
+    @IBOutlet var amountLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var borrowedReturnedLabel: UILabel!
+}
