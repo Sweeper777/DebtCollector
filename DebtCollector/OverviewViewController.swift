2e255e2da5d336b5e74b7dd9af83c0c968f0a13d
Sweeper777 <sumulang@gmail.com>
2018-07-16 13:35:06 +0100

    registered table view cells and stuff

--- DebtCollector/OverviewViewController.swift
+++ DebtCollector/OverviewViewController.swift
@@ -4,5 +4,12 @@ import RxCocoa
 import RxRealm
 
 class OverviewViewController: UITableViewController {
+    let disposeBag = DisposeBag()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = nil
+        tableView.delegate = nil
+        tableView.register(UINib(nibName: "OverviewTableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
+    }
 }
 
