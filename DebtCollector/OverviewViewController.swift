02bcd62686f4787efc596f1c42b12106d3355899
Sweeper777 <sumulang@gmail.com>
2018-07-17 08:37:08 +0100

    mpped transactions to [(Person, Double)]

--- DebtCollector/OverviewViewController.swift
+++ DebtCollector/OverviewViewController.swift
@@ -10,6 +10,20 @@ class OverviewViewController: UITableViewController {
         tableView.dataSource = nil
         tableView.delegate = nil
         tableView.register(UINib(nibName: "OverviewTableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
+        Observable.collection(from: RealmWrapper.shared.transactions)
+            .map {
+                transactions -> [(key: Person, value: Double)] in
+                var peopleDict = RealmWrapper.shared.people.reduce(into: [Person: Double](), { $0[$1] = 0 })
+                for transaction in transactions {
+                    if peopleDict[transaction.person!] != nil {
+                        peopleDict[transaction.person!]! += transaction.amount
+                    }
+                }
+                let mapped = peopleDict.map({ (x) in
+                    return x
+                })
+                return mapped
+            }
     }
 }
 
