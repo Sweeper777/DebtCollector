ba5b0ea05379dfd8eb3bb74af128c90933295f5d
Sweeper777 <sumulang@gmail.com>
2018-07-22 09:14:55 +0800

    added Transaction List VC

---
+++ DebtCollector/TransactionListViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+import RxCocoa
+import RxSwift
+import RxRealm
+import SwiftyUtils
+
+class TransactionListViewController: UITableViewController {
+    let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+    }
+}
