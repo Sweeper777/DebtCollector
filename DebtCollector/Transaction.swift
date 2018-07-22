8168ce44fc3d5acb7723f596cfcd6c8ac9809716
Sweeper777 <sumulang@gmail.com>
2018-07-23 07:49:26 +0800

    moved details property to Transaction class

--- DebtCollector/Transaction.swift
+++ DebtCollector/Transaction.swift
@@ -4,12 +4,12 @@ class Transaction: Object {
     @objc dynamic var amount: Double = 0.0
     @objc dynamic var personName = ""
     @objc dynamic var date = Date()
+    @objc dynamic var details = ""
     let parentTransactions = LinkingObjects(fromType: GroupTransaction.self, property: "transactions")
 }
 
 class GroupTransaction: Object {
     @objc dynamic var title = ""
-    @objc dynamic var details = ""
     let transactions = List<Transaction>()
 }
 
