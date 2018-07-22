256f926e813174cca52fcb5f163ac229dcc8358d
Sweeper777 <sumulang@gmail.com>
2018-07-22 09:14:25 +0800

    added GroupedTransactionTableViewCell.swift

---
+++ DebtCollector/GroupedTransactionTableViewCell.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+class GroupedTransactionTableViewCell : UITableViewCell {
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var transactionNameLabel: UILabel!
+    @IBOutlet var amountLabel: UILabel!
+}
