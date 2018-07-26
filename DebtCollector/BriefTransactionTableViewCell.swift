c84e975357589c776d7b4babcfc38cc752ce062a
Sweeper777 <sumulang@gmail.com>
2018-07-26 10:13:49 +0800

    added detail label in IB

--- DebtCollector/BriefTransactionTableViewCell.swift
+++ DebtCollector/BriefTransactionTableViewCell.swift
@@ -1,6 +1,7 @@
 import UIKit
 class BriefTransactionTableViewCell: UITableViewCell {
     @IBOutlet var transactionLabel: UILabel!
+    @IBOutlet var transactionDetailsLabel: UILabel!
     @IBOutlet var amountLabel: UILabel!
     @IBOutlet var dateLabel: UILabel!
     @IBOutlet var borrowedReturnedLabel: UILabel!
