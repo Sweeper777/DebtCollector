5fc34bbecdcb246234df935a8df6de5781f516b9
Sweeper777 <sumulang@gmail.com>
2018-07-27 08:40:45 +0800

    added DetailTransactionTableViewCell.swift

---
+++ DebtCollector/DetailTransactionTableViewCell.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class DetailTransactionTableViewCell: UITableViewCell {
+    @IBOutlet var transactionLabel: UILabel!
+    @IBOutlet var transactionDetailsLabel: UILabel!
+    @IBOutlet var amountLabel: UILabel!
+    @IBOutlet var borrowedReturnedLabel: UILabel!
+}
