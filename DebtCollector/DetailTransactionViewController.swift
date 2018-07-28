19515f3e88d8817dbe969313fcf6d4b1f1791af7
Sweeper777 <sumulang@gmail.com>
2018-07-28 08:38:55 +0800

    added DetailTransaction VC class

---
+++ DebtCollector/DetailTransactionViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+import RxCocoa
+import RxSwift
+import RxRealm
+import RxDataSources
+import SCLAlertView
+
+class DetailTransactionViewController : UITableViewController {
+    let disposeBag = DisposeBag()
+    
+    var groupedTransaction: GroupTransaction!
+    
+}
