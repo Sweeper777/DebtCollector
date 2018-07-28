6a3737c099df18e68e2f314d0fc876033a1e7216
Sweeper777 <sumulang@gmail.com>
2018-07-28 08:40:04 +0800

    created data source

--- DebtCollector/DetailTransactionViewController.swift
+++ DebtCollector/DetailTransactionViewController.swift
@@ -10,6 +10,38 @@ class DetailTransactionViewController : UITableViewController {
     
     var groupedTransaction: GroupTransaction!
     
+    override func viewDidLoad() {
+        tableView.dataSource = nil
+        tableView.delegate = nil
+        tableView.register(UINib(nibName: "DetailTransactionTableViewCell", bundle: nil), forCellReuseIdentifier: "transactionCell")
+        
+        let dataSource = RxTableViewSectionedReloadDataSource<DetailTransactionTableViewSection>(configureCell:  {
+            ds, tv, ip, item in
+            switch item {
+            case .transaction(let transaction):
+                let cell = tv.dequeueReusableCell(withIdentifier: "transactionCell") as! DetailTransactionTableViewCell
+                let formatter1 = NumberFormatter()
+                formatter1.numberStyle = .currency
+                cell.amountLabel.text = formatter1.string(from: abs(transaction.amount) as NSNumber)
+                cell.borrowedReturnedLabel.text = transaction.amount < 0 ? "Returned" : "Borrowed"
+                cell.transactionLabel.text = transaction.personName
+                cell.selectionStyle = .none
+                
+                if transaction.details.trimmed() != "" {
+                    cell.transactionDetailsLabel.text = transaction.details
+                } else {
+                    cell.transactionDetailsLabel.removeFromSuperview()
+                }
+                
+                return cell
+            case .button(title: let title):
+                let cell = tv.dequeueReusableCell(withIdentifier: "buttonCell")
+                cell!.textLabel!.text = title
+                return cell!
+            }
+        })
+        
+    }
 }
 
 enum DetailTransactionTableViewSection : SectionModelType {
