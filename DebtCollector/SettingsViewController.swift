b550aca0ffe61bdeacff84534eca460c2065daf8
Sweeper777 <sumulang@gmail.com>
2018-08-06 10:06:33 +0800

    removed the other detail preset option in settings vc

--- DebtCollector/SettingsViewController.swift
+++ DebtCollector/SettingsViewController.swift
@@ -6,27 +6,21 @@ class SettingsViewController : FormViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        form +++ Section(header: "detail presets (borrowing)", footer: "These are preset options that will show up when you start typing in the \"Details\" field when adding a borrowing transaction. Write one on each line.")
-        <<< TextAreaRow(tagDetailPresetsBorrowing) {
+        form +++ Section(header: "transaction detail presets", footer: "These are preset options that will show up when you start typing in the \"Details\" field. Write one on each line.")
+        <<< TextAreaRow(tagDetailPresets) {
             row in
-            row.value = UserSettings.detailPresetsForBorrowing
+            row.value = UserSettings.detailPresets
         }
         
-        form +++ Section(header: "detail presets (returning)", footer: "These are preset options that will show up when you start typing in the \"Details\" field when adding a returning transaction. Write one on each line.")
-        <<< TextAreaRow(tagDetailPresetsReturning) {
-            row in
-            row.value = UserSettings.detailPresetsForReturning
-        }
+        
     }
     
     @IBAction func done() {
         let values = form.values()
-        UserSettings.detailPresetsForBorrowing = (values[tagDetailPresetsBorrowing] as? String) ?? ""
-        UserSettings.detailPresetsForReturning = (values[tagDetailPresetsReturning] as? String) ?? ""
+        UserSettings.detailPresets = (values[tagDetailPresets] as? String) ?? ""
         
         self.dismiss(animated: true, completion: nil)
     }
 }
 
-let tagDetailPresetsBorrowing = "tagDetailPresetsBorrowing"
-let tagDetailPresetsReturning = "tagDetailPresetsReturning"
+let tagDetailPresets = "tagDetailPresets"
