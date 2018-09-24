2557ba1b953e5c5d3c038cbf12c49f4bed99a540
Sweeper777 <sumulang@gmail.com>
2018-09-24 17:57:20 +0100

    overridden setup

--- DebtCollector/TagCell.swift
+++ DebtCollector/TagCell.swift
@@ -4,4 +4,12 @@ import TagWriteView
 
 class TagCell: Cell<String>, CellType, TagWriteViewDelegate {
     @IBOutlet var tagWriteView: TagWriteView!
+    
+    override func setup() {
+        super.setup()
+        
+        tagWriteView.delegate = self
+        tagWriteView.placeHolderForInput = NSAttributedString(string: "Tags...")
+        tagWriteView.minimumWidthOfTag = 100
+    }
 }
