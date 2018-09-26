b2b339edbc3be626ff3e993a67d22113a9ef1c5b
Sweeper777 <sumulang@gmail.com>
2018-09-26 06:45:05 +0100

    filtered transactions depending on the existence of date

--- DebtCollector/TransactionListViewController.swift
+++ DebtCollector/TransactionListViewController.swift
@@ -15,21 +15,30 @@ class TransactionListViewController: UITableViewController {
         tableView.dataSource = nil
         tableView.register(UINib(nibName: "GroupedTransactionTableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
         
-        Observable.collection(from: RealmWrapper.shared.groupTransactions.sorted(byKeyPath: "date", ascending: false))
-            .bind(to: tableView.rx.items(cellIdentifier: "cell", cellType: GroupedTransactionTableViewCell.self)) {
-                index, groupTransaction, cell in
-                let totalAmount = abs(groupTransaction.transactions.map { $0.amount }.reduce(0, +))
-                let formatter1 = NumberFormatter()
-                formatter1.numberStyle = .currency
-                cell.amountLabel.text = formatter1.string(from: totalAmount as NSNumber)
-                
-                let formatter2 = DateFormatter()
-                formatter2.dateStyle = .short
-                formatter2.timeStyle = .none
-                cell.dateLabel.text = formatter2.string(from: groupTransaction.date)
-                cell.transactionNameLabel.text = groupTransaction.title
-            }
-            .disposed(by: disposeBag)
+        var observable: Observable<Results<GroupTransaction>>
+        if let date = self.date {
+            let start = Calendar.current.startOfDay(for: date)
+            let end = Calendar.current.date(byAdding: .day, value: 1, to: start)
+            observable = Observable.collection(from: RealmWrapper.shared.groupTransactions.sorted(byKeyPath: "date", ascending: false)
+                .filter("date BETWEEN %@", [start, end])
+            )
+        } else {
+            observable = Observable.collection(from: RealmWrapper.shared.groupTransactions.sorted(byKeyPath: "date", ascending: false))
+        }
+        observable.bind(to: tableView.rx.items(cellIdentifier: "cell", cellType: GroupedTransactionTableViewCell.self)) {
+            index, groupTransaction, cell in
+            let totalAmount = abs(groupTransaction.transactions.map { $0.amount }.reduce(0, +))
+            let formatter1 = NumberFormatter()
+            formatter1.numberStyle = .currency
+            cell.amountLabel.text = formatter1.string(from: totalAmount as NSNumber)
+            
+            let formatter2 = DateFormatter()
+            formatter2.dateStyle = .short
+            formatter2.timeStyle = .none
+            cell.dateLabel.text = formatter2.string(from: groupTransaction.date)
+            cell.transactionNameLabel.text = groupTransaction.title
+        }
+        .disposed(by: disposeBag)
         
         tableView.rx.modelSelected(GroupTransaction.self).subscribe(onNext: {
             [weak self] groupTransaction in
