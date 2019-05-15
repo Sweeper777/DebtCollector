fccd100b7b045d07aeaaa90b8e5d6566e6ec4dc6
Sweeper777 <sumulang@gmail.com>
2019-05-15 07:12:37 +0100

    update schema version

--- DebtCollector/AppDelegate.swift
+++ DebtCollector/AppDelegate.swift
@@ -17,9 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         FirebaseApp.configure()
         
         let config = Realm.Configuration(
-            schemaVersion: 2,
+            schemaVersion: 3,
             migrationBlock: { migration, oldSchemaVersion in
-                if (oldSchemaVersion < 2) {
+                if (oldSchemaVersion < 3) {
                 }
         })
         Realm.Configuration.defaultConfiguration = config
