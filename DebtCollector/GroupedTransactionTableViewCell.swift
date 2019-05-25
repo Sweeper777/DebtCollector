067e2aeabef08fb6af38ccb4be0d0800f16a87f4
Sweeper777 <sumulang@gmail.com>
2019-05-25 09:18:06 +0100

    removed date label in grouped transaction cell

--- DebtCollector/GroupedTransactionTableViewCell.swift
+++ DebtCollector/GroupedTransactionTableViewCell.swift
@@ -2,7 +2,6 @@ import UIKit
 import MGSwipeTableCell
 
 class GroupedTransactionTableViewCell : MGSwipeTableCell {
-    @IBOutlet var dateLabel: UILabel!
     @IBOutlet var transactionNameLabel: UILabel!
     @IBOutlet var amountLabel: UILabel!
 }
