6d57d9c6ef780b5f97dd252fa2f902dcc6c84897
Sweeper777 <sumulang@gmail.com>
2019-10-28 08:20:25 +0000

    added aggregateTransactionAndPeople

--- Debtors/TodayViewController.swift
+++ Debtors/TodayViewController.swift
@@ -21,6 +21,19 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     }
     func reload() {
     }
+    
+    func aggregateTransactionAndPeople(transactions: Results<Transaction>, people: Results<Person>) -> [(key: String, value: Double)] {
+        var peopleDict = people.reduce(into: [String: Double](), { $0[$1.name] = 0 })
+        for transaction in transactions {
+            if peopleDict[transaction.personName] != nil {
+                peopleDict[transaction.personName]! += transaction.amount.value ?? 0
+            }
+        }
+        let mapped = peopleDict.map({ (x) in
+            return x
+        })
+        return mapped.sorted(by: { ($0.value, $1.key) > ($1.value, $0.key) })
+    }
         
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
         // Perform any setup necessary in order to update the view.
