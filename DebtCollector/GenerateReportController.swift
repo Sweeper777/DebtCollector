2504b93e7844359eebb12630a1db874e7121894c
Mulang Su <sumulang@gmail.com>
2019-12-09 14:29:04 +0000

    added date range options

--- DebtCollector/GenerateReportController.swift
+++ DebtCollector/GenerateReportController.swift
@@ -10,3 +10,14 @@ class GenerateReportController: FormViewController {
         super.viewDidLoad()
         
     }
+
+// MARK: Date Range Options
+
+fileprivate let last7Days = "Last 7 Days"
+fileprivate let last14Days = "Last 14 Days"
+fileprivate let last30Days = "Last 30 Days"
+fileprivate let last90Days = "Last 90 Days"
+fileprivate let last365Days = "Last 365 Days"
+fileprivate let allTime = "All Time"
+fileprivate let custom = "Custom"
+
