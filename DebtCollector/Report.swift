d938524008b6352302bcd76adc84862578879d24
Mulang Su <sumulang@gmail.com>
2019-12-10 15:20:08 +0000

    added a bunch of properties

--- DebtCollector/Report.swift
+++ DebtCollector/Report.swift
@@ -2,4 +2,11 @@ import Foundation
 
 struct Report {
 
+    let totalBorrows: Double
+    let totalReturns: Double
+    let netBalance: Double
+    
+    let borrowsByPerson: [(String, Double)]
+    let returnsByPerson: [(String, Double)]
+    
 }
