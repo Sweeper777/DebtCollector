fef55caec99b4a90361e9b4aaacfe53c623038c3
Mulang Su <sumulang@gmail.com>
2019-12-11 16:33:58 +0000

    added outlets

--- DebtCollector/ReportController.swift
+++ DebtCollector/ReportController.swift
@@ -5,5 +5,12 @@ class ReportController : UITableViewController {
     
     var report: Report!
     
+    @IBOutlet var totalBorrowingsLabel: UILabel!
+    @IBOutlet var totalReturnsLabel: UILabel!
+    @IBOutlet var netBalanceLabel: UILabel!
+    @IBOutlet var netBalanceTextLabel: UILabel!
+    @IBOutlet var topBorrowersBarChart: BarChartView!
+    @IBOutlet var topReturnersBarChart: BarChartView!
+    
 }
 
