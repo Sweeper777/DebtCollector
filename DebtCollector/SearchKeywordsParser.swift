e81a50749a87df8f9b7ccf1ae7e797e7a14ec2b1
Mulang Su <sumulang@gmail.com>
2020-01-06 10:34:47 +0000

    finished SearchKeywordsParser

--- DebtCollector/SearchKeywordsParser.swift
+++ DebtCollector/SearchKeywordsParser.swift
@@ -10,5 +10,22 @@ struct SearchKeywordsParser {
         var currentKeyword = ""
         var keywords = [String]()
         
+        func newKeyword() {
+            keywords.append(currentKeyword)
+            currentKeyword = ""
+        }
+        
+        for char in string {
+            if char == delimiterAND {
+                inAnd.toggle()
+                newKeyword()
+            } else if char == delimiterOR && !inAnd {
+                newKeyword()
+            } else {
+                currentKeyword += "\(char)"
+            }
+        }
+        keywords.append(currentKeyword)
+        return keywords.filter { !$0.isEmpty }
     }
 }
